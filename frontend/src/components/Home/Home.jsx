1d2fe4bea56376d909c23a4e4b57150db4f0abe7
Xandesh Dahal <dahalsandesh55@gmail.com>
2024-06-10 22:27:01 +0545

    design changes

--- frontend/src/components/Home/Home.jsx
+++ frontend/src/components/Home/Home.jsx
@@ -111,23 +111,27 @@ const HomePage = () => {
           <>
             <div className="w-full lg:w-3/5 lg:pr-6 mb-8 lg:mb-0">
               <div className="flex flex-col lg:flex-row lg:items-center justify-between mb-4">
-                <h2 className="text-2xl font-bold mb-4 lg:mb-0">Recommended News</h2>
+                <h2 className="text-2xl font-bold mb-8 lg:mb-8">Recommended News</h2>
                 {mediumScreen && (
-                  <div className="flex items-center mb-4 lg:mb-0">
-                    <input
-                      type="text"
-                      placeholder="Search news..."
-                      value={searchQuery}
-                      onChange={(e) => setSearchQuery(e.target.value)}
-                      className="border border-gray-300 p-2 rounded-lg"
-                    />
-                    <button
-                      onClick={handleSearch}
-                      className="bg-blue-500 text-white p-2 rounded-lg ml-2 hover:bg-blue-700"
-                    >
-                      Search
-                    </button>
-                  </div>
+                  <div class="flex items-center mb-6">
+                  <input
+                    type="text"
+                    placeholder="Search news..."
+                    value={searchQuery}
+                    onChange={(e) => setSearchQuery(e.target.value)}
+                    class="border border-gray-300 px-4 py-2 rounded-lg w-full lg:w-4/5 shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 transition"
+                  />
+                  <button
+                    type="submit"
+                    onClick={handleSearch}
+                    class="bg-blue-500 text-white px-4 py-2 rounded-lg mt-0 lg:mt-0 lg:ml-2 hover:bg-blue-700 shadow-sm flex items-center justify-center transition transform hover:scale-105 lg:w-1/5"
+                  >
+                    <svg class="h-5 w-5 mr-2 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 56.966 56.966">
+                      <path d="M55.146,51.887L41.588,37.786c3.486-4.144,5.396-9.358,5.396-14.786c0-12.682-10.318-23-23-23s-23,10.318-23,23  s10.318,23,23,23c4.761,0,9.298-1.436,13.177-4.162l13.661,14.208c0.571,0.593,1.339,0.92,2.162,0.92  c0.779,0,1.518-0.297,2.079-0.837C56.255,54.982,56.293,53.08,55.146,51.887z M23.984,6c9.374,0,17,7.626,17,17s-7.626,17-17,17  s-17-7.626-17-17S14.61,6,23.984,6z"></path>
+                    </svg>
+                    Search
+                  </button>
+                </div>
                 )}
               </div>
               <div className="grid grid-cols-1 gap-4">
@@ -154,28 +158,33 @@ const HomePage = () => {
                 )}
               </div>
             </div>
-            <div className="w-full lg:w-1/3 lg:pl-6">
+            <div className="w-full lg:w-1/3 lg:pl-4">
               <div className="flex flex-col">
                 <div className="mb-4">
                   {!mediumScreen && (
-                    <div className="flex items-center mb-4">
-                      <input
-                        type="text"
-                        placeholder="Search news..."
-                        value={searchQuery}
-                        onChange={(e) => setSearchQuery(e.target.value)}
-                        className="border border-gray-300 p-2 rounded-lg w-full lg:w-auto"
-                      />
-                      <button
-                        onClick={handleSearch}
-                        className="bg-blue-500 text-white p-2 rounded-lg mt-2 lg:mt-0 lg:ml-2 hover:bg-blue-700 w-full lg:w-auto"
-                      >
-                        Search
-                      </button>
-                    </div>
+                  <div class="flex items-center mb-4">
+                  <input
+                    type="text"
+                    placeholder="Search news..."
+                    value={searchQuery}
+                    onChange={(e) => setSearchQuery(e.target.value)}
+                    class="border border-gray-300 px-4 py-2 rounded-lg w-full lg:w-3/4 shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 transition"
+                  />
+                  <button
+                    type="submit"
+                    onClick={handleSearch}
+                    class="bg-blue-500 text-white px-4 py-2 rounded-lg mt-2 lg:mt-0 lg:ml-2 hover:bg-blue-700 shadow-sm flex items-center justify-center transition transform hover:scale-105 w-full lg:w-1/4"
+                  >
+                    <svg class="h-5 w-5 mr-2 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 56.966 56.966">
+                      <path d="M55.146,51.887L41.588,37.786c3.486-4.144,5.396-9.358,5.396-14.786c0-12.682-10.318-23-23-23s-23,10.318-23,23  s10.318,23,23,23c4.761,0,9.298-1.436,13.177-4.162l13.661,14.208c0.571,0.593,1.339,0.92,2.162,0.92  c0.779,0,1.518-0.297,2.079-0.837C56.255,54.982,56.293,53.08,55.146,51.887z M23.984,6c9.374,0,17,7.626,17,17s-7.626,17-17,17  s-17-7.626-17-17S14.61,6,23.984,6z"></path>
+                    </svg>
+                    Search
+                  </button>
+                </div>
+                 
                   )}
                 </div>
-                <h2 className="text-2xl font-bold mb-4">Trending</h2>
+                <h2 className="text-2xl font-bold mb-6">Trending</h2>
                 <div className="grid grid-cols-1 gap-4">
                   {currentLatestNews.map((news) => (
                     <TrendingNewsCard
@@ -192,22 +201,26 @@ const HomePage = () => {
         ) : (
           <div className="w-full">
             <div className="flex justify-between items-center mb-4">
-              <h2 className="text-2xl font-bold">Latest News</h2>
-              <div className="flex items-center">
-                <input
-                  type="text"
-                  placeholder="Search news..."
-                  value={searchQuery}
-                  onChange={(e) => setSearchQuery(e.target.value)}
-                  className="border border-gray-300 p-2 rounded-lg"
-                />
-                <button
-                  onClick={handleSearch}
-                  className="bg-blue-500 text-white p-2 rounded-lg ml-2 hover:bg-blue-700"
-                >
-                  Search
-                </button>
-              </div>
+              <h2 className="text-2xl font-bold mb-6">Latest News</h2>
+              <div class="flex items-center mb-4">
+                  <input
+                    type="text"
+                    placeholder="Search news..."
+                    value={searchQuery}
+                    onChange={(e) => setSearchQuery(e.target.value)}
+                    class="border border-gray-300 px-4 py-2 rounded-lg w-full lg:w-3/4 shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 transition"
+                  />
+                  <button
+                    type="submit"
+                    onClick={handleSearch}
+                    class="bg-blue-500 text-white px-4 py-2 rounded-lg mt-2 lg:mt-0 lg:ml-2 hover:bg-blue-700 shadow-sm flex items-center justify-center transition transform hover:scale-105 w-full lg:w-1/4"
+                  >
+                    <svg class="h-5 w-5 mr-2 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 56.966 56.966">
+                      <path d="M55.146,51.887L41.588,37.786c3.486-4.144,5.396-9.358,5.396-14.786c0-12.682-10.318-23-23-23s-23,10.318-23,23  s10.318,23,23,23c4.761,0,9.298-1.436,13.177-4.162l13.661,14.208c0.571,0.593,1.339,0.92,2.162,0.92  c0.779,0,1.518-0.297,2.079-0.837C56.255,54.982,56.293,53.08,55.146,51.887z M23.984,6c9.374,0,17,7.626,17,17s-7.626,17-17,17  s-17-7.626-17-17S14.61,6,23.984,6z"></path>
+                    </svg>
+                    Search
+                  </button>
+                </div>
             </div>
             <div className="grid grid-cols-1 gap-8">
               {currentLatestNews.map((news) => (
