9083fe0c6b804d2c57c87b6d3a547d20253dce4f
Xandesh Dahal <dahalsandesh55@gmail.com>
2024-06-14 22:21:06 +0545

    video updates..

--- frontend/src/components/NewsCard/VideoCard.jsx
+++ frontend/src/components/NewsCard/VideoCard.jsx
@@ -13,13 +13,14 @@ const VideoCard = ({ title, videoUrl, description }) => {
 
 
   return (
-    <div className="flex flex-col justify-center items-center border border-gray-200 rounded-lg overflow-hidden shadow-md transition-transform duration-300 transform hover:scale-105 hover:shadow-xl mb-8 hover:glow p-4 bg-white">
+    <div className="flex flex-col justify-center items-center border border-gray-200 rounded-lg overflow-hidden shadow-md transition-transform duration-300 transform hover:scale-105 hover:shadow-xl mb-14 hover:glow p-4 bg-white">
       <h3 className="text-lg font-bold mb-4">{title}</h3>
       <video
         ref={videoRef}
-        className="object-cover w-2/3 rounded-lg mb-4"
+        className="object-cover  rounded-lg mb-4"
         src={videoUrl}
-        style={{ aspectRatio: '16 / 9' }}
+        style={{ aspectRatio: '1.85 / 1' }}
+        autoPlay={false}
         loop
         onMouseEnter={(e) => e.currentTarget.play()}
         onMouseLeave={(e) => e.currentTarget.pause()}
