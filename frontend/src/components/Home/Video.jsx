ef896b7c1ebd9aa15f88dc00d42e5f80f1bdb387
Xandesh Dahal <dahalsandesh55@gmail.com>
2024-06-14 08:53:13 +0545

    updates..

--- frontend/src/components/Home/Video.jsx
+++ frontend/src/components/Home/Video.jsx
@@ -9,7 +9,7 @@ const Videos = () => {
   useEffect(() => {
     const fetchVideos = async () => {
       try {
-        const response = await axios.get('http://127.0.0.1:8000/api/main/get_videos');
+        const response = await axios.get('http://127.0.0.1:8000/api/vapps/');
         setVideos(response.data);
       } catch (error) {
         console.error('Error fetching videos:', error);
